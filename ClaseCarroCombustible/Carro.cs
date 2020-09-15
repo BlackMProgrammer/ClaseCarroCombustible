ca3dec99ae2c891099dd46e54ac1f7bce3a47756
BlackMProgrammer <cristhiancastro01@gmail.com>
2020-09-15 11:05:00 -0400

    Add project files.

---
+++ ClaseCarroCombustible/Carro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClaseCarroCombustible
+{
+    class Carro
+    {
+        private int _cantidadCombustible;
+
+        public Carro(int _cantidadCombustible) {
+            this._cantidadCombustible = _cantidadCombustible;
+        }
+        public void encender() {
+            if (_cantidadCombustible > 0)
+            {
+                Console.WriteLine("El Carro fue encendido");
+                _cantidadCombustible--;
+                Console.WriteLine("Gasolina restante {0}", _cantidadCombustible);
+            }
+            else {
+                Console.WriteLine("El carro no pudo avanzar");
+            }
+            
+        }
+    }
+}
---
+++ ClaseCarroCombustible/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClaseCarroCombustible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la marca del vehiculo que posee: ");
+            string marca = Console.ReadLine();
+
+            Console.WriteLine("Digite la cantidad de galones que posee:");
+            string galonesval = Console.ReadLine();
+            int galones = Convert.ToInt32(galonesval);
+
+            Carro carro = new Carro(galones);
+            Console.WriteLine("El modelo del vehiculo es {0}", marca);
+            do
+            {
+                carro.encender();
+                galones--;
+            } while (galones > -1);
+            
+        }
+    }
+}
